5275139dc48b6b9c1ed51e391d17040f6db16008
VicFrolov <vicfrolov@gmail.com>
2015-01-30 17:37:23 -0800

    added all methods. non functional.

--- DateCounter.java
+++ DateCounter.java
@@ -1,6 +1,30 @@
 public class DateCounter {
 
+	public static boolean isLeapYear(int year){
+		return true;
+	}
+
+	public static int daysInMonth(int year, int month) {
+		return 5;
+	}
+
+	public static boolean isValidDate(int year, int month, int day) {
+		return true;
+	}
+
+	public static int daysBetween(int year0, int month0, int day0, int year1, int month1, int day1) {
+		return 5;
+	}
+
+	public static boolean hasLeapSecond(int year) {
+		return true;
+	}
+
+	public static boolean hasLeapSecond(int year, int month, int day) {
+		return true;
+	}
+
 	public static void main(String[] args) {
-		return;
+		return ;
 	}
 }
