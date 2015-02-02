22e20c28b5ae56356383a73c272e93289dec102a
VicFrolov <vicfrolov@gmail.com>
2015-02-01 19:59:51 -0800

    Added daysinmonth and is validdate method functionality

--- DateCounter.java
+++ DateCounter.java
@@ -1,8 +1,8 @@
 public class DateCounter {
 
     public static boolean isLeapYear(int year){
-        boolean isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-        if (isLeapYear && year > 1583) {
+        boolean isLeapYear = (((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) && year > 1583);
+        if (isLeapYear) {
             return true;
         } else {
             return false;
@@ -10,11 +10,38 @@ public class DateCounter {
     }
 
     public static int daysInMonth(int year, int month) {
-        return 5;
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+            return 31;
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            return 30;
+        } else  if (month == 2) {
+            if (isLeapYear(year) == true) {
+                return 29;
+            } else {
+                return 28;
+            }            
+        } else {
+            // not sure why the hell it won't work without the return 100
+            return -1;
+        }
     }
 
     public static boolean isValidDate(int year, int month, int day) {
-        return true;
+        if (month > 12 || month < 1 || day > 31 || day < 1) {
+            return false;
+        } else if (month == 2) {
+            if (isLeapYear(year) == true && day > 29) {
+                return false;
+            } else if (isLeapYear(year) == false && day > 28) {
+                return false;
+            } else {
+                return true;
+            }
+        } else if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     public static int daysBetween(int year0, int month0, int day0, int year1, int month1, int day1) {
