a9763fad75596613185a634dcc6a25be6ab4b0eb
VicFrolov <vicfrolov@gmail.com>
2015-02-02 00:33:38 -0800

    messed around a lot, found some apis. 6/8 test harness date between working

--- DateCounter.java
+++ DateCounter.java
@@ -1,12 +1,9 @@
+import java.util.Calendar;
+
 public class DateCounter {
 
     public static boolean isLeapYear(int year){
-        boolean isLeapYear = (((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) && year > 1583);
-        if (isLeapYear) {
-            return true;
-        } else {
-            return false;
-        }
+        return  (((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) && year > 1583);
     }
 
     public static int daysInMonth(int year, int month) {
@@ -19,9 +16,8 @@ public class DateCounter {
                 return 29;
             } else {
                 return 28;
-            }            
+            }
         } else {
-            // not sure why the hell it won't work without the return 100
             return -1;
         }
     }
@@ -45,7 +41,49 @@ public class DateCounter {
     }
 
     public static int daysBetween(int year0, int month0, int day0, int year1, int month1, int day1) {
-        return 5;
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+
+        cal1.set(year0, month0, day0);
+        cal2.set(year1, month1, day1);
+
+        long milis1 = cal1.getTimeInMillis();
+        long milis2 = cal2.getTimeInMillis();
+
+        long diff = milis2 - milis1;
+
+        int diffDays = (int) (diff / (24 * 60 * 60 * 1000));
+
+
+        /*int totalDays = 0;
+        int earlierDate;
+        int newerDate;
+        if (year0 > year1) {
+            earlierDate = day1;
+            newerDate = day0;
+        } else if (year1 > year0) {
+            earlierDate = day0;
+            newerDate = day1;
+        } else {
+            if (month0 > month1) {
+                earlierDate = day0;
+                newerDate = day1;
+            } else if (month1 > month 0) {
+                earlierDate = year
+            }
+        }
+
+        while(smallerYear < biggerYear) {
+            if (isLeapYear(smallerYear)) {
+                totalDays += 366;
+            } else {
+                totalDays += 365;
+            }
+            smallerYear += 1;
+        }
+        return totalDays;
+        */
+        return diffDays;
     }
 
     public static boolean hasLeapSecond(int year) {
