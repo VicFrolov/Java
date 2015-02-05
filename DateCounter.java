d3a0c8c0ef17ca6743217cf11b3679089d6cd286
VicFrolov <vicfrolov@gmail.com>
2015-02-04 20:12:35 -0800

    fixed spacing. FINAL.

--- DateCounter.java
+++ DateCounter.java
@@ -1,10 +1,12 @@
 public class DateCounter {
 
     public static boolean isLeapYear(int year){
+        
         return  (((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) && year > 1583);
     }
 
     public static int daysInMonth(int year, int month) {
+
         if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
             return 31;
         } else if (month == 4 || month == 6 || month == 9 || month == 11) {
@@ -21,6 +23,7 @@ public class DateCounter {
     }
 
     public static boolean isValidDate(int year, int month, int day) {
+
         if (month > 12 || month < 1 || day > 31 || day < 1) {
             return false;
         } else if (month == 2) {
@@ -39,6 +42,7 @@ public class DateCounter {
     }
 
     public static int daysBetween(int year0, int month0, int day0, int year1, int month1, int day1) {
+
         int totalDays = 0;
         int earlierYear = 0;
         int earlierMonth = 0;
@@ -116,6 +120,7 @@ public class DateCounter {
     }
 
     public static boolean hasLeapSecond(int year) {
+
         int[] leapYears = {1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979, 1981, 1982, 1983, 1985, 1987, 1989, 1990, 1992, 1993, 1994, 1995, 1997, 1998, 2005, 2008, 2012, 2015};
 
         for(int i = 0; i < leapYears.length; i++ ) {
@@ -127,6 +132,7 @@ public class DateCounter {
     }
 
     public static boolean hasLeapSecond(int year, int month, int day) {
+
         int[] hasLeapFirstHalf = {1972, 1981, 1982, 1983, 1985, 1992, 1993, 1994, 1997, 2012, 2015};
         int[] hasLeapSecondHalf = {1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979, 1987, 1989, 1990, 1995, 1998, 2005, 2008};
 
@@ -137,6 +143,7 @@ public class DateCounter {
                 }
             }
         }
+
         if (month == 12 && day == 31) {
             for(int i = 0; i < hasLeapSecondHalf.length; i++ ) {
                 if(year == hasLeapSecondHalf[i]) {
@@ -183,7 +190,6 @@ public class DateCounter {
         }
 
         int result = daysBetween(year0, month0, day0, year1, month1, day1);
-
         System.out.println(result);
     }
 }
