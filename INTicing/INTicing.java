4a229f9b54ca1a68983bf9c26e34881e6c4e22e7
VicFrolov <vicfrolov@gmail.com>
2015-04-04 16:17:32 -0700

    fully functional double decimal string method helper, and add one helper

--- INTicing/INTicing.java
+++ INTicing/INTicing.java
@@ -15,20 +15,20 @@ public class INTicing {
         boolean isNumberNotZero = true;
         boolean numberisNotZero = true;
         //find the index of the first digit of the number in the String array
-        while(testforZerosAndSigns) {
-            if(indexToCheck == tempStringArray.length -1 && tempStringArray[tempStringArray.length -1].equals("0")) {
+        while (testforZerosAndSigns) {
+            if (indexToCheck == tempStringArray.length -1 && tempStringArray[tempStringArray.length -1].equals("0")) {
                 this.binaryDigits = new byte[]{0};
                 numberisNotZero = false;
                 testforZerosAndSigns = false;
-            } else if(tempStringArray[indexToCheck].equals("0") || tempStringArray[indexToCheck].equals("+") || tempStringArray[indexToCheck].equals("-")) {
+            } else if (tempStringArray[indexToCheck].equals("0") || tempStringArray[indexToCheck].equals("+") || tempStringArray[indexToCheck].equals("-")) {
                 indexToCheck++;
             } else {
                 testforZerosAndSigns = false;
             }
         }
-        if(numberisNotZero){
+        if (numberisNotZero){
             //check if the number is positive or negative
-            if(tempStringArray[0].equals("-")) {
+            if (tempStringArray[0].equals("-")) {
             this.isPositiveNumber = -1;
             } else {
             this.isPositiveNumber = 1;
@@ -36,17 +36,17 @@ public class INTicing {
             //create an array of inversed decimal digits
             int[] decimalDigits = new int[tempStringArray.length - (indexToCheck)];
 
-            for(int i = tempStringArray.length -1; i >= indexToCheck; i--) {
+            for (int i = tempStringArray.length -1; i >= indexToCheck; i--) {
                 decimalDigits[j] = Integer.parseInt(tempStringArray[i]);
                 j++;
             }
             //divide by two, and make new binary String array
-            while(isNumberNotZero) {
+            while (isNumberNotZero) {
                 int sum = 0;
-                for(int i = 0; i < decimalDigits.length; i++) {
+                for (int i = 0; i < decimalDigits.length; i++) {
                     sum += decimalDigits[i];
                 }
-                if(sum == 0) {
+                if (sum == 0) {
                     isNumberNotZero = false;
                 } else if(decimalDigits[0] % 2 == 0) {
                     tempBinaryString = "0" + tempBinaryString;  
@@ -59,7 +59,7 @@ public class INTicing {
             String[] tempBinaryStringArray = tempBinaryString.split("(?!^)");
 
             this.binaryDigits = new byte[tempBinaryStringArray.length];
-            for(int i =0 ; i < tempBinaryStringArray.length; i++) {
+            for (int i =0 ; i < tempBinaryStringArray.length; i++) {
                 this.binaryDigits[i] = Byte.parseByte(tempBinaryStringArray[i]);   
             }
         }
@@ -71,11 +71,11 @@ public class INTicing {
 
         //special case: if the int array is of the number 1
         //otherwise, appropriate length is derived
-        if(number.length == 1 && number[0] == 1) {
+        if (number.length == 1 && number[0] == 1) {
             number = new int[1];
             number[0] = 0;
             return number;
-        } else if(number[number.length-1] == 1) {
+        } else if (number[number.length-1] == 1) {
             specificLength = number.length -1;
         } else {
             specificLength = number.length;
@@ -83,11 +83,11 @@ public class INTicing {
         int[] returnByte = new int[specificLength];
 
         //Calculate half
-        for(int i=0; i+1 < number.length; i++) {
-            if(number[i] % 2 != 0){
+        for (int i=0; i+1 < number.length; i++) {
+            if (number[i] % 2 != 0){
                 number[i] -= 1;
-            }
-            if(number[i+1] % 2 !=0) {
+            } 
+            if (number[i+1] % 2 !=0) {
                 number[i] = number[i] / 2 + 5;
             } else {
                 number[i] /= 2;
@@ -95,61 +95,46 @@ public class INTicing {
         }
         number[number.length-1] /= 2;
         //convert to bytes
-        for(int i = 0; i < specificLength; i++) {
+        for (int i = 0; i < specificLength; i++) {
             returnByte[i] = number[i];
         }
+        
         return number;
     }
 
-    // public String toString() {
-
-    //  int totalCount = 0;
-    //  String stringDigits = "";
-
-    //  for(int i= 0; i < this.binaryDigits.length; i++) {
-    //      totalCount *= 2;
-    //      if(this.binaryDigits[i] == 1) {
-    //          totalCount +=1;
-    //      }
-    //  }
-
-    //  if(this.isPositiveNumber == 1){
-    //      stringDigits = "+";
-    //  } else if(this.isPositiveNumber == -1){
-    //      stringDigits = "-";
-    //  }
-    //  stringDigits = stringDigits + totalCount + "";
-    //  return stringDigits;
-
-    // }
-
-
-    // public String toString() {
-    //     String stringDigits = "0";
+    public String toString() {
+        String stringDigits = "0";
         
-    //     if(binaryDigits[0] == 0 && binaryDigits.length == 1) {
-    //         return stringDigits;
-    //     }
+        if(binaryDigits[0] == 0 && binaryDigits.length == 1) {
+            return stringDigits;
+        }
         
-    //     stringDigits = "1";
+        stringDigits = "1";
 
-    //     for(int i = 1; i < this.binaryDigits.length; i++) {
-    //         stringDigits = doubleDecimalString(stringDigits);
-    //         if(binaryDigits[i] == 1) {
-    //             stringDigits = addOne(stringDigits);
+        // for(int i = 1; i < this.binaryDigits.length; i++) {
+        //     stringDigits = doubleDecimalString(stringDigits);
+        //     if(binaryDigits[i] == 1) {
+        //         stringDigits = addOne(stringDigits);
+        //     }
+        //     System.out.println("string print leap : " + stringDigits);
+        // }
 
-    //         }
-    //     }
+        for(int i = this.binaryDigits.length -1; i >= 1; i--) {
+        System.out.println("string print leap : " + stringDigits);
+            stringDigits = doubleDecimalString(stringDigits);
+            if(binaryDigits[i] == 1) {
+                stringDigits = addOne(stringDigits);
+            }
+        }
 
-    //     if(this.isPositiveNumber == 1){
-    //         stringDigits = "+" + stringDigits;
-    //     } else if(this.isPositiveNumber == -1){
-    //         stringDigits = "-" + stringDigits;
-    //     }
-    //         System.out.println(stringDigits);
+        if(this.isPositiveNumber == 1){
+            stringDigits = "+" + stringDigits;
+        } else if(this.isPositiveNumber == -1){
+            stringDigits = "-" + stringDigits;
+        }
 
-    //     return stringDigits;
-    // }
+        return stringDigits;
+    }
 
 
 
@@ -158,53 +143,84 @@ public class INTicing {
         int[] doubledArray = new int[revIntArrayOfString.length + 1];
         String doubledString = "";
         boolean carry = false;
+        boolean carryChecker = false;
 
-        for(int i = 0; i < revIntArrayOfString.length; i++) {
+        for (int i = 0; i < revIntArrayOfString.length; i++) {
             revIntArrayOfString[i] = Character.getNumericValue(s.charAt(s.length() -1 -i));
         }
-        for(int i = 0; i < doubledArray.length - 1; i++) {
-            if(revIntArrayOfString[i] <=4){
+
+        for (int i = 0; i < doubledArray.length -1; i++) {
+            if (revIntArrayOfString[i] <=4){
                 doubledArray[i] = revIntArrayOfString[i] * 2;
+                carryChecker = false;
             }
             else {
                 doubledArray[i] = revIntArrayOfString[i] * 2 - 10;
-                doubledArray[i +1] += 1;
+                carryChecker = true;
             }
-            for(int j=0; j < doubledArray.length; j++) {
-                System.out.println("LOL" + doubledArray[j]);
+            if (carry) {
+                doubledArray[i]++;
+                carry = false;
             }
-            
+            carry = carryChecker;
         }
 
-        for(int i=0; i < doubledArray.length; i++) {
-            System.out.println(doubledArray[i]);
+        if (revIntArrayOfString[revIntArrayOfString.length-1] >= 5) {
+            doubledArray[doubledArray.length -1] = 1;
         }
 
-        if(doubledArray[doubledArray.length-1] == 0) {
-            for(int i = doubledArray.length -2; i >= 0; i--) {
+        if (doubledArray[doubledArray.length-1] == 0) {
+            for (int i = doubledArray.length -2; i >= 0; i--) {
                 doubledString += (char)(doubledArray[i] + 48);                
             }
         } else {
             for(int i = doubledArray.length -1; i >= 0; i--) {
                 doubledString += (char)(doubledArray[i] + 48);
             }
-        } 
-        System.out.println("string: " + doubledString);
+        }
+
         return doubledString;
     }
 
 
 
     public static String addOne(String s) {
-        String[] digits = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-        for(int i = 0; i < digits.length; i++) {
-            if(s == digits[i] && i <9){
-                return digits[i+1];
-            } else if(s == digits[i] && i == 9) {
-                return digits[0];
+        int[] revIntArrayOfString = new int[s.length()];
+        int[] arrayPlusOne = new int[revIntArrayOfString.length + 1];
+        String plusOneString = "";
+
+        for (int i = 0; i < revIntArrayOfString.length; i++) {
+            revIntArrayOfString[i] = Character.getNumericValue(s.charAt(s.length() -1 -i));
+        }
+        for (int i = 0; i < revIntArrayOfString.length; i++) {
+            arrayPlusOne[i] = revIntArrayOfString[i];
+        }
+
+        if (arrayPlusOne[0] < 9) {
+            arrayPlusOne[0]++;
+        } else {
+            for (int i= 0; i < arrayPlusOne.length - 1; i++) {
+                if (arrayPlusOne[i+1] < 9) {
+                    arrayPlusOne[i] = 0;
+                    arrayPlusOne[i +1]++;
+                    break;
+                } else {
+                    arrayPlusOne[i] = 0;
+                }
             }
         }
-        return null;
+
+        if (arrayPlusOne[arrayPlusOne.length-1] == 0) {
+            for (int i = arrayPlusOne.length -2; i >= 0; i--) {
+                plusOneString += (char)(arrayPlusOne[i] + 48);                
+            }
+        } else {
+            for (int i = arrayPlusOne.length -1; i >= 0; i--) {
+                plusOneString += (char)(arrayPlusOne[i] + 48);
+            }
+        }
+
+        return plusOneString;
     }
 
 
