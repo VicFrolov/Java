3fbc2767aff7b772469af7c86511615edb78b3c8
VicFrolov <vicfrolov@gmail.com>
2015-04-12 01:54:30 -0700

    Piestimator fully functional, still missing final printed out line. also must fully fix so it's not all static with terrible instance variables and bad helper methods

--- estimators/PiEstimator.java
+++ estimators/PiEstimator.java
@@ -1,26 +1,49 @@
 public class PiEstimator {
-    private float timesHit = 0;
-    private float timesMissed = 0;
+    static int timesHit = 0;
+    static int timesMissed = 0;
 
 
     public static void dartThrow(int dartAmount) {
         double x = 0.0;
         double y = 0.0;
 
-        for(int j = 0; j < dartAmount; j++) {
-            x = Math.random() * 2.0 -1.0;
-            y = Math.random() * 2.0 -1.0;
-            System.out.println(x);
-            System.out.println(y);
+        System.out.println("start");
 
+        for(int j = 0; j < dartAmount; j++) {
+            boolean hit = false;
+            x = Math.random() * 2.0 - 1.0;
+            y = Math.random() * 2.0 - 1.0;
+            hit = ((x*x + y*y) <= 1);
+
+            if (Math.pow(x, 2.0) + Math.pow(y, 2.0) <= 1) {
+                timesHit++;
+            } else {
+                timesMissed++;
+            }
+
+            System.out.print(x + " ");
+            System.out.print(y);
+            
+            if (hit) {
+                System.out.print(" " + "in\n");
+            } else {
+                System.out.print(" " + "out\n");
+            }
         }
+        
+        System.out.println("end");
+
     }
 
 
 
     public static void main(String[] args) {
-        dartThrow(100);
 
-    }   
+        if(args.length == 0) {
+            dartThrow(10000);
+        } else {
+            dartThrow(Integer.parseInt(args[0]));
+        }
+    }
 
 }
