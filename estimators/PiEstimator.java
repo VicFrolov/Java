511fc091b13963a4f2b3b4bc4b8a52fa3f01d459
VicFrolov <vicfrolov@gmail.com>
2015-04-12 02:47:08 -0700

    Removed redundant print statements. formatting. removed useless variables. Added clear square value rather than arbitrary number. formatting fixes and spacing

--- estimators/PiEstimator.java
+++ estimators/PiEstimator.java
@@ -1,45 +1,34 @@
 public class PiEstimator {
-    static int timesHit = 0;
-    static int timesMissed = 0;
-
+    private static double timesHit = 0.0;
 
     public static void dartThrow(int dartAmount) {
         double x = 0.0;
         double y = 0.0;
+        int squareArea = 4;
 
         System.out.println("start");
 
-        for(int j = 0; j < dartAmount; j++) {
-            boolean hit = false;
+        for (int j = 0; j < dartAmount; j++) {
             x = Math.random() * 2.0 - 1.0;
             y = Math.random() * 2.0 - 1.0;
-            hit = ((x*x + y*y) <= 1);
+            boolean hit = ((x*x + y*y) <= 1);
 
-            if (Math.pow(x, 2.0) + Math.pow(y, 2.0) <= 1) {
-                timesHit++;
-            } else {
-                timesMissed++;
-            }
+            System.out.print(x + " " + y);
 
-            System.out.print(x + " ");
-            System.out.print(y);
-            
             if (hit) {
+                timesHit++;
                 System.out.print(" " + "in\n");
             } else {
                 System.out.print(" " + "out\n");
             }
         }
         
-        System.out.println("end");
-
+        System.out.println("end\n" + "Darts: " + dartAmount + " Hits: " + timesHit + 
+            " Pi estimate: " +  timesHit / dartAmount * squareArea);
     }
 
-
-
     public static void main(String[] args) {
-
-        if(args.length == 0) {
+        if (args.length == 0) {
             dartThrow(10000);
         } else {
             dartThrow(Integer.parseInt(args[0]));
