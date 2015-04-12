60571e127b3734c66f4b1ac4f47c4eae5e04d55c
VicFrolov <vicfrolov@gmail.com>
2015-04-12 00:50:28 -0700

    basic skeleton, non functional.

---
+++ estimators/PieEstimator.java
@@ -0,0 +1,17 @@
+public class PiEstimator {
+	private float timesHit = 0;
+	private float timesMissed = 0;
+
+
+	public String dartThrow(int i) {
+
+	}
+
+
+
+	public static void main(String[] args) {
+		
+
+	}	
+
+}
