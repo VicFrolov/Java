91e9775f5ea444cf0148c5c0b86421e0a5a44dbd
VicFrolov <vicfrolov@gmail.com>
2015-04-13 19:03:41 -0700

    added a boundCalculator with fudge factor to find the lower and upper bounds

--- estimators/Polynomial.java
+++ estimators/Polynomial.java
@@ -1,12 +1,10 @@
  public class Polynomial {
-    private static Double[] reversedCoefficients;
-
+    private Double[] reversedCoefficients;
+    private Double solvedValue = 0.0;
 
     public Polynomial(Double[] n) {
         int j = 0;
         reversedCoefficients = new Double[n.length];
-
-
         for(int i = n.length - 1 ; i >=0; i--) {
             this.reversedCoefficients[j] = n[i];
             j++;
@@ -14,13 +12,12 @@
     }
 
 
-    public static double Solver(Double x) {
-        Double solvedValue = 0.0; 
-
+    public double solver(Double x) {
+        solvedValue = 0.0;
         for(int i = 0; i < reversedCoefficients.length; i++) {
             solvedValue += Math.pow(x, i) * reversedCoefficients[i];
         }
         return solvedValue;
     }
-    
+
 }
