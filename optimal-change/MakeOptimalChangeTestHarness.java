8874119b7dc73393b3aa81a9437c16eb157141ab
VicFrolov <vicfrolov@gmail.com>
2015-04-21 16:11:38 -0700

    added some text cases for imposisble and random numbers

--- optimal-change/MakeOptimalChangeTestHarness.java
+++ optimal-change/MakeOptimalChangeTestHarness.java
@@ -8,8 +8,9 @@ public class MakeOptimalChangeTestHarness {
         successes = 0;
 
         test_USA();
-        // Add more!
-
+        test_CAD();
+        test_IMPOSSIBLE();
+        test_random();
         System.out.println(successes + "/" + attempts + " tests passed.");
     }
 
@@ -24,12 +25,51 @@ public class MakeOptimalChangeTestHarness {
         displaySuccessIfTrue(false);
     }
 
+    public static void test_CAD() {
+        int[] cadDenominations = new int[] {5, 10, 25, 100, 200};
+        Tally resultCad = MakeOptimalChange.makeOptimalChange(cadDenominations, 215);
+
+
+        try {
+            displaySuccessIfTrue(1 == resultCad.getElement(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(1 == resultCad.getElement(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(0 == resultCad.getElement(2));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+        
+        try {
+            displaySuccessIfTrue(0 == resultCad.getElement(3));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
+
+        try {
+            displaySuccessIfTrue(1 == resultCad.getElement(4));
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }        
+
+
+    }
     public static void test_USA() {
         int[] usaDenominations = new int[] { 25, 10, 5, 1 };
-        int[] cadDenominations = new int[] {5, 10, 25, 100, 200};
-
         Tally result = MakeOptimalChange.makeOptimalChange(usaDenominations, 99);
-        Tally resultCad = MakeOptimalChange.makeOptimalChange(cadDenominations, 217);
 
         try {
             displaySuccessIfTrue(3 == result.getElement(0));
@@ -60,42 +100,54 @@ public class MakeOptimalChangeTestHarness {
         }
 
 
+    }
 
+
+    public static void test_random() {
+        int[] usaDenominations = new int[] { 2, 3, 9, 100 };
+        Tally result = MakeOptimalChange.makeOptimalChange(usaDenominations, 32);
 
         try {
-            displaySuccessIfTrue(1 == resultCad.getElement(4));
+            displaySuccessIfTrue(1 == result.getElement(0));
         } catch (Exception e) {
             e.printStackTrace();
             displayFailure();
         }
 
         try {
-            displaySuccessIfTrue(1 == resultCad.getElement(1));
+            displaySuccessIfTrue(1 == result.getElement(1));
         } catch (Exception e) {
             e.printStackTrace();
             displayFailure();
         }
 
         try {
-            displaySuccessIfTrue(1 == resultCad.getElement(0));
-        } catch (Exception e) {
-            e.printStackTrace();
-            displayFailure();
-        }
-        
-        try {
-            displaySuccessIfTrue(0 == resultCad.getElement(2));
+            displaySuccessIfTrue(3 == result.getElement(2));
         } catch (Exception e) {
             e.printStackTrace();
             displayFailure();
         }
 
         try {
-            displaySuccessIfTrue(0 == resultCad.getElement(3));
+            displaySuccessIfTrue(0 == result.getElement(3));
         } catch (Exception e) {
             e.printStackTrace();
             displayFailure();
-        }        
+        }
+
+
+    }
+
+    public static void test_IMPOSSIBLE() {
+        int[] usaDenominations = new int[] { 25, 10, 5, 2 };
+        Tally result = MakeOptimalChange.makeOptimalChange(usaDenominations, 1);
+
+        try {
+            displaySuccessIfTrue(result.isImpossible());
+        } catch (Exception e) {
+            e.printStackTrace();
+            displayFailure();
+        }
 
     }
 
