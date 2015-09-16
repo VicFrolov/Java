cf4fd2d86640dda8f26628d22a735a89687d1df4
Victor Frolov <vicfrolov@gmail.com>
2015-09-15 17:36:42 -0700

    added a converter from any base to decimal

--- BaseConverter/BaseConverter.java
+++ BaseConverter/BaseConverter.java
@@ -1,7 +1,28 @@
 public class BaseConverter {
+	// private long decimalRepresentation = 0;
+	// private int fromBase = 0;
+	// private int toBase = 0;
+	// private byte[] numberToConvert;
 	
+	// public BaseConverter(byte b, int i, int j) {
+	// 	b = this.numberToConvert;
+	// 	i = this.fromBase;
+	// 	j = this.toBase;
+	// }
+
+	public long conversionToDecimal(byte[] numberToConvert, int base) {
+		long decimalValueSoFar = 0;
+		
+		for (int i = 0; i < numberToConvert.length; i++) {
+			decimalValueSoFar += numberToConvert[i] * Math.pow(base, i);
+		}
+		return decimalValueSoFar;
+	}
+
+
+
 	public static void main(String[] args) {
-	
+
 	}
 
 }
